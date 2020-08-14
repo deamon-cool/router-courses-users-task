1da47054004f1c4211c6017a77adeb2a0fb3d52b
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-14 18:54:24 +0200

    added style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import './App.css';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
