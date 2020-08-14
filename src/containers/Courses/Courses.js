64aedf5c8e3dafd11b26517ba8109a5ae30d8a17
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-14 19:11:58 +0200

    added clickable Course

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 
 import './Courses.css';
+import Course from '../Course/Course';
 
 class Courses extends Component {
     state = {
@@ -11,17 +13,25 @@ class Courses extends Component {
         ]
     }
 
-    render () {
+
+
+    render() {
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
                     {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
+                        this.state.courses.map(course => {
+                            return (
+                                <Link to={'/courses/' + course.id} >
+                                    <article className="Course" key={course.id}>{course.title}</article>
+                                </Link>
+                            );
+                        })
                     }
                 </section>
+                <Route path='/courses/:id' component={Course} />
+
             </div>
         );
     }
