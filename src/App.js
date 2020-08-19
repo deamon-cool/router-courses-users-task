862059c494cfbdf31927c3766e16d9b8fbda26bb
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-19 18:47:48 +0200

    added page not found with controling pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 import Courses from './containers/Courses/Courses';
@@ -15,17 +15,15 @@ class App extends Component {
             <li><NavLink to='/users'>Users</NavLink></li>
           </ul>
 
-          <Route path='/courses' component={Courses} />
-          <Route path='/users' component={Users} />
+          <Switch>
+            <Route path='/' exact render={() => <h1>Main Page</h1>} />
+            <Route path='/courses' component={Courses} />
+            <Route path='/users' exact component={Users} />
+            <Route path='/not-found' exact render={() => <h1>404 Not Found</h1>} />
+            <Redirect to='/not-found' />
+          </Switch>
 
           <ol style={{ textAlign: 'left' }}>
-            <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-            <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-            <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-            <li>Pass the course ID to the "Course" page and output it there</li>
-            <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-            <li>Load the "Course" component as a nested component of "Courses"</li>
-            <li>Add a 404 error page and render it for any unknown routes</li>
             <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
           </ol>
         </div>
