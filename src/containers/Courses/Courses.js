70bcb954d49104a1d0b212d2c50a60db8d1e7d1e
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-19 18:06:26 +0200

    added params that are receiving by Course component

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -10,10 +10,15 @@ class Courses extends Component {
             { id: 1, title: 'Angular - The Complete Guide' },
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
-        ]
+        ],
+        choosenTitle: null
     }
 
+    clickHandle = (id) => {
+        const course = this.state.courses.find(course => course.id === id);
 
+        this.setState({ choosenTitle: course.title });
+    }
 
     render() {
         return (
@@ -23,14 +28,15 @@ class Courses extends Component {
                     {
                         this.state.courses.map(course => {
                             return (
-                                <Link to={'/courses/' + course.id} >
-                                    <article className="Course" key={course.id}>{course.title}</article>
+                                <Link onClick={() => this.clickHandle(course.id)} to={'/courses/' + course.id} key={course.id}>
+                                    <article className="Course">{course.title}</article>
                                 </Link>
                             );
                         })
                     }
                 </section>
-                <Route path='/courses/:id' component={Course} />
+                <Route path='/courses/:id' render={(props) => <Course {...props} title={this.state.choosenTitle} />} />
+                {/* <Route path='/courses/:id' component={Course} /> */}
 
             </div>
         );
